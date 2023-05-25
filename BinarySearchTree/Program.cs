5fb4b374ebb88836768d1b0e071e9245aa120f3c
anilac13 <anil4u2496@gmail.com>
2023-05-25 19:58:50 +0530

    [Anil] Add:Create binary search tree with the given data

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -9,6 +9,17 @@ namespace BinarySearchTree
             tree.Add(56);
             tree.Add(30);
             tree.Add(70);
+            tree.Add(22);
+            tree.Add(40);
+            tree.Add(60);
+            tree.Add(95);
+            tree.Add(11);
+            tree.Add(65);
+            tree.Add(3);
+            tree.Add(16);
+            tree.Add(63);
+            tree.Add(67);
+
             tree.Print();
         }
     }
